548dcefa58c0d77298bb5c587dba62df274075f8
Bedelici Adelina <adelinabedelici@gmail.com>
2026-04-24 19:57:25 +0300

    Add: verificare ce task rezolvam prin numele fisierelor

--- gigiquant/src/Task1.c
+++ gigiquant/src/Task1.c
@@ -9,7 +9,14 @@ int main(int argc, const char *argv[])
         printf("Eroare la deschiderea fisierelor!");
         return 1;
     }
-
+    const char *fisiere[]={ "data1.in", "data2.in", "data3.in", "data4.in", "data5.in", "data6.in", "data7.in", "data8.in", "data9.in", "data10.in","data11.in", "data12.in", "data13.in", "data14.in", "data15.in", "data16.in", "data17.in", "data18.in", "data19.in", "data20.in"};
+    int task=0;
+    for(int i=0; i<=19; i++){
+        if(strcmp(argv[1], fisiere[i])==0){
+            task = i/5 + 1 ;
+            break;}
+    }
+    if(task==1){
     /// Introduceti numarul total de observatii al portofoliului studiat
     int nrZile, i;
     fscanf(fi, "%d", &nrZile);
@@ -42,7 +49,11 @@ int main(int argc, const char *argv[])
     fprintf(fo,"%.3lf\n", ((int)(randMediu*1000))/1000.0);
     fprintf(fo, "%.3lf\n", ((int)(volatilitate*1000))/1000.0);
     fprintf(fo, "%.3lf\n", ((int)(S*1000))/1000.0);
-    stergereLista(&cap_lista);
+    stergereLista(&cap_lista); }
+
+    if(task == 2){
+
+    }
     fclose(fi);
     fclose(fo);
     return 0;
